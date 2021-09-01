0ca2531d3f04317a820662557617e474737aa453
a12345 <wimm251280@icloud.com>
2021-09-01 22:31:29 +0300

    ok

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,39 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.*;
+
+
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+//    public static Connection connectDB() {
+//
+//        final String password = "12345678";
+//        final String user = "root";
+//        final String url = "jdbc:mysql://localhost:3306/userdb";
+//
+//        try {
+//            Connection connection = DriverManager.getConnection(url, user, password);
+//            System.out.println("Соединение установлено");
+//        } catch (SQLException e) {
+//            System.out.println("Ошибка соединения");
+//        }
+//        return ;
+//    }
+public static Connection connectDB() {
+    String userName = "root";
+    String password = "12345678";
+    String url = "jdbc:mysql://localhost:3306/userdb";
+
+    try {
+        return DriverManager.getConnection(url, userName, password);
+    } catch (SQLException e) {
+        e.printStackTrace();
+        throw new IllegalStateException();
+    }
 }
+}
+
+
+
